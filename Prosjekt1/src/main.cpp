2fb28c8b00f3a788dae3adade7de502dc01fc5c4
Jon Jahren <jon.jahren@gmail.com>
2019-11-22 19:55:57 +0100

    Noen endringer

--- Prosjekt1/src/main.cpp
+++ Prosjekt1/src/main.cpp
@@ -1,9 +1,13 @@
-#include <Arduino.h
+#include <Arduino.h>
 
 void setup() {
+
+Serial.begin(9600);
 
 }
 
 void loop() {
+Serial.print("Skriv inn et regnestykke");
 
+while(!Serial.available()) {}
 }
